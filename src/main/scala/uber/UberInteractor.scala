a8d755b6c09c521f76d18c1cdc4f2ae7f9d5ca1e
Kul <singhal.kul@gmail.com>
2015-10-08 11:28:02 +0530

    support for paginating implicitly in case some trips might not be on first page

--- src/main/scala/uber/UberInteractor.scala
+++ src/main/scala/uber/UberInteractor.scala
@@ -4,31 +4,28 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.concurrent.TimeUnit._
 
-import org.openqa.selenium.{WebElement, WebDriver, By}
-import org.openqa.selenium.firefox.{FirefoxProfile, FirefoxDriver}
 import org.openqa.selenium.interactions.Actions
+import org.openqa.selenium.support.ui.{ExpectedConditions, WebDriverWait}
+import org.openqa.selenium.{By, WebElement}
+import uber.Trip.TripDateOrdering
 
 import scala.collection.JavaConverters._
-import scala.collection.mutable
 
 /**
  * Author: Kul
  */
-case class UberInteractor(userName: String, password: String, downloadDir: String) {
+case class UberInteractor(userName: String, password: String, driverProvider: DriverProvider) {
 
-  val driver = new FirefoxDriver(firefoxProfile(downloadDir))
+  val driver = driverProvider.driver
+
   driver.manage().timeouts().implicitlyWait(3, SECONDS)
 
   driver.get("https://login.uber.com/login")
   login()
 
   def trips(from: LocalDate, to: LocalDate, paymentMethod: String) = {
-    val tripTable = driver.findElement(By.id("trips-table")).findElement(By.tagName("tbody"))
-    val trips = tripTable.findElements(By.cssSelector("tr.trip-expand__origin")).asScala
-    trips.map(buildTrip)
-      .filter(filterOutCanceledTrips)
-      .filter(t => t.date.isAfter(from) && t.date.isBefore(to))
-      .filter(filterBasedOnPaymentMethod(_, paymentMethod))
+    val trips = getTripsWithinDateRange(1, from, to, Nil)
+    trips.filter(filterOutCanceledTrips).filter(filterBasedOnPaymentMethod(_, paymentMethod))
   }
 
   def downloadInvoices(trips: Seq[Trip]) {
@@ -36,21 +33,34 @@ case class UberInteractor(userName: String, password: String, downloadDir: Strin
     driver.quit()
   }
 
-  private def firefoxProfile(receiptDownloadDir: String) = {
-    val profile = new FirefoxProfile()
-    profile.setPreference("browser.download.folderList", 2)
-    profile.setPreference("browser.download.dir", receiptDownloadDir)
-    profile.setPreference("browser.download.manager.showWhenStarting", false)
-    profile.setPreference("browser.helperApps.neverAsk.saveToDisk", "application/pdf")
-    profile.setPreference("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream")
-    profile
+  private def getTripsWithinDateRange(page: Int, from: LocalDate, to: LocalDate, previousTrips: Seq[Trip]): Seq[Trip] = {
+    gotoPage(page)
+    val filteredTripsFromPage = tripsFromPage.filter(t => dateBetween(t.date, from, to))
+    val allTrips = (filteredTripsFromPage ++ previousTrips).sorted
+    if(from.isBefore(allTrips.head.date)) getTripsWithinDateRange(page + 1, from, to, allTrips) else allTrips
+  }
+
+  private def tripsFromPage = {
+    val tripTable = driver.findElement(By.id("trips-table")).findElement(By.tagName("tbody"))
+    val trips = tripTable.findElements(By.cssSelector("tr.trip-expand__origin")).asScala.map(buildTrip)
+    trips
+  }
+
+  private def dateBetween(date: LocalDate, from: LocalDate, to: LocalDate) = date.isEqual(from) || date.isEqual(to) || (date.isAfter(from) && date.isBefore(to))
+
+  private def gotoPage(page: Int): Unit = {
+    if (page > 1) {
+      driver.findElement(By.className("pagination__next")).click()
+      while (driver.findElement(By.id("trips-next-loader")).getAttribute("style").nonEmpty) {
+        Thread.sleep(100)
+      }
+    }
   }
 
   private def login() {
     driver.findElement(By.id("email")).sendKeys(userName)
     driver.findElement(By.id("password")).sendKeys(password)
     driver.findElement(By.cssSelector("button[type='submit']")).click()
-    println("Logged in")
   }
   
   private def filterOutCanceledTrips(trip: Trip) = !trip.fare.contains("Canceled")
@@ -71,13 +81,10 @@ case class UberInteractor(userName: String, password: String, downloadDir: Strin
   private def downloadInvoice(trip: Trip) {
     val tripId = trip.id
     driver.get(s"https://riders.uber.com/trips/$tripId")
-    val downloadButton = driver.findElements(By.xpath("//button[text()='Download Invoice']")).asScala
-
-    if(downloadButton.nonEmpty) {
-      val act = new Actions(driver)
-      act.moveToElement(downloadButton.head)
-      downloadButton.head.click()
-      println(s"Downloaded for $tripId")
-    }
+    val downloadButton = driver.findElement(By.xpath("//button[text()='Download Invoice']"))
+    new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOf(downloadButton))
+    val act = new Actions(driver)
+    act.moveToElement(downloadButton)
+    downloadButton.click()
   }
 }
