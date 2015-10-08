2492e155ad9edc68d9f9c27aba5976e9881abfac
Kul <singhal.kul@gmail.com>
2015-10-08 11:26:36 +0530

    added trip date ordering functionality

--- src/main/scala/uber/Trip.scala
+++ src/main/scala/uber/Trip.scala
@@ -6,3 +6,11 @@ import java.time.LocalDate
  * Author: Kul
  */
 case class Trip(id: String, date: LocalDate, fare: String, city: String, paymentMethod: String)
+
+object Trip {
+
+  implicit object TripDateOrdering extends Ordering[Trip] {
+    override def compare(x: Trip, y: Trip) = x.date.compareTo(y.date)
+
+  }
+}
